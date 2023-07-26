535b6e8a93f23a76361eb589a1850c58cb427468
FunkyWood <120262069+FunkyWood@users.noreply.github.com>
2023-07-26 15:33:52 +0100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  site: 'https://funkywood.github.io'
+  site: 'https://funkywood.github.io',
+  base: '/supershll'
+  
 });
